1de22314a42996f7e7d354ad53c00ebb5407a750
Zenghong Chen <czhherry@gmail.com>
2020-04-07 19:24:08 +0800

    update waypoint updater

--- 12_path_following_part_2/waypoint_updater/scripts/waypoint_updater.py
+++ 12_path_following_part_2/waypoint_updater/scripts/waypoint_updater.py
@@ -53,22 +53,6 @@ class WaypointUpdater(object):
 
         closest_idx = self.waypoint_tree.query([x,y],1)[1]
 
-
-
-
-        # Check if closest is ahead or behind vehicle
-        closest_coord = self.waypoints_2d[closest_idx]
-        prev_coord = self.waypoints_2d[closest_idx-1]
-
-        # Equation for hyperplane through closest_coords
-        cl_vect = np.array(closest_coord)
-        prev_vect = np.array(prev_coord)
-        pos_vect = np.array([x, y])
-
-        val = np.dot(cl_vect-prev_vect, pos_vect-cl_vect)
-
-        if val > 0:
-            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
         return closest_idx          
 
     def publish_waypoints(self, closest_idx):
